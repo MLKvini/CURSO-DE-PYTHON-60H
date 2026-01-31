d113c06eb02a536f7f408fd4dbcc93a522738ca2
Vinicius P <vinicius.rochasherri7638@gmail.com>
2026-01-31 17:44:19 -0300

    Add files via upload

---
+++ aula_6.py
@@ -0,0 +1,23 @@
+# indentação  -  posicionei as declarações corretamente
+
+
+# print('teste')
+# if 10 > 2:
+#     pass
+# print('é maior')
+
+
+
+# refatoração -  alinhar os erros do código 
+# bug 
+
+
+if 10 < 2:
+    print('certo')
+else:
+    print('errado')    
+
+
+
+# concatenação
+# juntar  nome, 'text
