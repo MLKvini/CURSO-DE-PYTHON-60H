0dda11c18e346be7ee89b30ed272ab3513bb3e2b
Vinicius P <vinicius.rochasherri7638@gmail.com>
2026-01-31 14:34:06 -0300

    Add files via upload

---
+++ CALCULADORA.py
@@ -0,0 +1,24 @@
+#CRIAR UMA CALCULADORA COM A FUNÇÃO INPUT E OUTPUT(print())
+#PRECISA TER AS 4 OPERAÇÕES
+#soma
+#subtração
+#multiplicação
+#divisão
+#UTILIZAR
+#VARIÁVEIS, SINAIS ARITMÉTICOS. 
+print('calculadora python')
+
+num1 = float(input('coloca algum numero: '))
+num2 = float(input('coloca outro numero: '))
+
+soma = num1 + num2
+print('ESSE É O RESULTADO DA SOMA:',soma)
+
+menos = num1 - num2
+print('ESSE É O RESULTADO DA SUBTRAÇÃO:',menos)
+
+multiplicacao = num1 * num2
+print('ESSE É O RESULTADO DA MULTIPLICACÃO:',multiplicacao)
+
+divisao = num1 / num2
+print('ESSE É O RESULTADO DIVISÃO:',divisao)
