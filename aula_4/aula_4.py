3bfc8c51bb3e7601e54173bbe395a3012ee39daf
Vinicius P <vinicius.rochasherri7638@gmail.com>
2026-01-31 15:48:32 -0300

    Add files via upload

---
+++ aula_4/aula_4.py
@@ -0,0 +1,25 @@
+#ATIVIDADES:
+#1 - Crie um programa para efetuar a leitura de um número inteiro e apresentar o resultado do quadrado deste número.
+
+numero = 10
+print(numero ** 2)
+print('-'*30)
+#2 - Crie duas variáveis para armazenar seu primeiro nome e sobrenome. Em seguida, concatene-as para formar seu nome completo e exiba o resultado.
+nome = ('vinicius')
+sobrenome = ('pires')
+print(nome , sobrenome)
+print('-'*30)
+#3 - Peça ao usuário para digitar dois números inteiros e armazene-os em variáveis. Realize a concatenação desses números como strings e exiba o resultado.
+num1 = int(input('COLOCA ALGUM NUMERO:'))
+num2 = int(input('COLOCA OUTRO NUMERO:'))
+print("ESSES SÃO OS NUMEROS:", num1 , num2 )
+print('-'*30)
+#4 - Crie uma variável para armazenar a palavra "Python". Em seguida, adicione um número inteiro ao final da palavra usando a concatenação e exiba o resultado.
+palavra = ('python')
+print('palavra,',45)
+print('-'*30)
+#5 - Declare uma variável contendo uma frase. Em seguida, peça ao usuário para digitar uma palavra e concatene essa palavra no final da frase. Exiba o resultado. 
+fra = ('TERRARIA É UM JOGO')
+nome = input('digite seu nome:')
+print(fra + nome)
+print('-'*30)
